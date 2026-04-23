84aeb45b83434eddf35d143fdcad13a2962cbdc8
cipheroot <cipheroot@proton.me>
2026-04-23 10:44:21 +0530

    implemented billing with clerk

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { PricingTable } from "@clerk/nextjs";
 
 const Subscription = () => {
-  return <div>Subscription</div>;
+  return <div>
+    <PricingTable />
+  </div>;
 };
 
 export default Subscription;
